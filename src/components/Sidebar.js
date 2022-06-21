978cc93cbf959fdd42184b908511a429631cebc5
Omar <omar.assed@gmail.com>
2022-06-21 16:09:22 -0600

    adding new notes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,28 @@
 import React from "react";
 import NoteList from "./NoteList";
 
-function Sidebar({ notes, handleClick }) {
+function Sidebar({ notes, handleClick, handleAddNote }) {
+
+  function addNote() {
+    const newNote = {title: 'draft title', body: 'draft body'}
+  fetch('http://localhost:3000/notes', {
+    method: 'POST', 
+    headers: { 'Content-type': 'application/json'},
+    body: JSON.stringify(
+      newNote
+    )
+  }
+  )
+    .then(r=>r.json())
+    .then(data=>  handleAddNote(data))
+  }
 
   return (
     <div className="master-detail-element sidebar">
       <NoteList notes = {notes} handleClick = {handleClick}/>
-      <button>New</button>
+      <button onClick = {addNote}>New</button>
     </div>
   );
-}
+  }
 
 export default Sidebar;
