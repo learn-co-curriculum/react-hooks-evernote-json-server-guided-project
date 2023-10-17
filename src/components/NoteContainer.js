d4856a3e87062c70fc97d9d431012d882c0f07c6
Emily Valdez <emily.milan.valdez@gmail.com>
2023-10-17 09:01:31 -0500

    added initial state and note fetch

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 
 function NoteContainer() {
+
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/notes")
+    .then(r => r.json())
+    .then(notes => setNotes(notes))
+  },[])
+
+  console.log(notes)
+
   return (
     <>
       <Search />
